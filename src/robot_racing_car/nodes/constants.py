e746f6333544e1168a2d43134cb4b3dbe61b8bc8
Bogdan Djukic <bdjukic@gmail.com>
2017-05-23 00:47:08 +0200

    Adding constants file for sharing them across the project.

---
+++ src/robot_racing_car/nodes/constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Steering commands
+MOVE_FORWARD_COMMAND = 0
+MOVE_BACK_COMMAND = 1
+TURN_LEFT_COMMAND = 2
+TURN_RIGHT_COMMAND = 3
+BREAK_COMMAND = 4
+
+# Camera image manipulation
+IMAGE_SCALE = 4
