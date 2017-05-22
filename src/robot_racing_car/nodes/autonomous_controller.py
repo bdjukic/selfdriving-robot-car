061b6d91518f8b78b6149a37a4305b03f523d08d
Bogdan Djukic <bdjukic@gmail.com>
2017-05-23 00:46:43 +0200

    Improving the control of the vehicle in the autonomous mode.

--- src/robot_racing_car/nodes/autonomous_controller.py
+++ src/robot_racing_car/nodes/autonomous_controller.py
@@ -3,37 +3,55 @@
 import rospy
 import cv2
 import numpy as np
+import tensorflow as tf
+import h5py
+import constants.py
 
 from std_msgs.msg import UInt8
-
 from sensor_msgs.msg import CompressedImage
-from cv_bridge import CvBridge, CvBridgeError
-
 from keras.models import load_model
-import h5py
+
 
 class AutonomousController:
-    bridge = CvBridge()
-    model = load_model('../model/model.h5')
 
-    def image_callback(self, message):
-        try:
-	    np_arr = np.fromstring(message.data, np.uint8)
-            image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-    	except CvBridgeError, e:
-            rospy.logerror(e)
-    	else: 
-	    image_array = np.asarray(image)
-            steering_command = int(model.predict(image_array[None, :, :, :], batch_size=1))
-	    self.robot_car_steering_publisher.publish(UInt8(steering_command))
+    model = load_model("../model/model.h5")
+    graph = tf.get_default_graph()
+    last_image = None
+
+    def camera_callback(self, message):
+        np_arr = np.fromstring(message.data, np.uint8)
+        image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        small_image = np.empty((image.shape[1] / constants.IMAGE_SCALE, image.shape[0] / constants.IMAGE_SCALE))
+        image = cv2.resize(image, small_image.shape, interpolation=cv2.INTER_LINEAR)
+
+        self.last_image = np.asarray(image)
+
+    def steering_callback(self, callback_argument):
+        if self.last_image is None:
+            return
+
+        with self.graph.as_default():
+            steering_command = self.model.predict(self.last_image[None, :, :, :], batch_size=1)
+            steering_command = np.argmax(steering_command)
+            rospy.logdebug("Steering command: " + str(steering_command))
+
+            self.robot_car_steering_publisher.publish(steering_command)
+
+            if steering_command == constants.TURN_LEFT_COMMAND or steering_command == constants.TURN_RIGHT_COMMAND:
+                rospy.sleep(0.1)
+                self.robot_car_steering_publisher.publish(constants.MOVE_FORWARD_COMMAND)
+                rospy.sleep(0.2)
 
     def __init__(self):
-        rospy.init_node('camera_subscriber', log_level=rospy.DEBUG)
-        rospy.Subscriber('/raspicam_node/image/compressed', CompressedImage, self.image_callback, queue_size = 1)
+        rospy.init_node("autonomous_controller", log_level=rospy.DEBUG)
 
-        self.robot_car_steering_publisher = rospy.Publisher('robot_car_steering', UInt8, queue_size=10)
+        rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, self.camera_callback, queue_size=1)
+        self.robot_car_steering_publisher = rospy.Publisher("robot_car_steering", UInt8, queue_size=1)
 
-if __name__ == '__main__':
+        rospy.Timer(rospy.Duration(0.25), self.steering_callback)
+
+
+if __name__ == "__main__":
     controller = AutonomousController()
-    
+
     rospy.spin()
