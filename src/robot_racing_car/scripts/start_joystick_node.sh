a6e40f9b3cdd5d177774e3538169a6ffae2938d6
Bogdan Djukic <bdjukic@gmail.com>
2017-05-22 23:57:05 +0200

    Making sure joystick commands are re-emitted after buttons are pressed.

--- src/robot_racing_car/scripts/start_joystick_node.sh
+++ src/robot_racing_car/scripts/start_joystick_node.sh
@@ -2,7 +2,10 @@
 
 # This is the script for launching joystick node on the master ROS machine
 
-echo Starting ROS Serial node:
+echo Starting ROS Joystick node:
 echo
+
+# Making sure joystick is re-emitting message for the last pressed button
+rosparam set /joy_node/autorepeat_rate 1.0
 
 rosrun joy joy_node
