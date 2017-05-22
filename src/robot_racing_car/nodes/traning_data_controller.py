df4712c67d282dee80e4cccc81c623b34659fd9c
Bogdan Djukic <bdjukic@gmail.com>
2017-05-23 00:47:55 +0200

    Little clean up.

--- src/robot_racing_car/nodes/traning_data_controller.py
+++ src/robot_racing_car/nodes/traning_data_controller.py
@@ -6,67 +6,60 @@ import numpy as np
 import csv
 import time
 import os
+import constants.py
 
 from sensor_msgs.msg import CompressedImage
-from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Joy
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+
 
 class TrainingDataController:
-    MOVE_FORWARD_COMMAND = 0
-    MOVE_BACK_COMMAND = 1
-    TURN_LEFT_COMMAND = 2
-    TURN_RIGHT_COMMAND = 3
-    BREAK_COMMAND = 4
+    def approx_time_synchronizer_callback(self, image_message, joystick_message):
+        if joystick_message.axes[6] == -0.0 and joystick_message.axes[7] == -0.0:
+            # Ignore BREAK_COMMAND while getting training data
+            return
 
-    bridge = CvBridge()
-    last_joystick_command = BREAK_COMMAND
+        if joystick_message.axes[6] == 1.0:
+            last_joystick_command = constants.TURN_LEFT_COMMAND
+        elif joystick_message.axes[6] == -1.0:
+            last_joystick_command = constants.TURN_RIGHT_COMMAND
+        elif joystick_message.axes[7] == 1.0:
+            last_joystick_command = constants.MOVE_FORWARD_COMMAND
+        elif joystick_message.axes[7] == -1.0:
+            last_joystick_command = constants.MOVE_BACK_COMMAND
 
-    def joystick_callback(self, message):
-        if (message.axes[6] == 1.0):
-            self.last_joystick_command = self.TURN_LEFT_COMMAND
+        np_arr = np.fromstring(image_message.data, np.uint8)
+        image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        small_image = np.empty((image.shape[1] / constants.IMAGE_SCALE, image.shape[0] / constants.IMAGE_SCALE))
+        last_image = cv2.resize(image, small_image.shape, interpolation=cv2.INTER_LINEAR)
 
-        if (message.axes[6] == -1.0):
-            self.last_joystick_command = self.TURN_RIGHT_COMMAND
+        training_data_folder = "../training_data/"
+        training_data_file_path = training_data_folder + "training_data.csv"
 
-        if (message.axes[7] == 1.0):
-            self.last_joystick_command = self.MOVE_FORWARD_COMMAND
+        append_write_flag = "w"
 
-        if (message.axes[7] == -1.0):
-            self.last_joystick_command = self.MOVE_BACK_COMMAND
+        if os.path.exists(training_data_file_path):
+            append_write_flag = "a"
 
-        if (message.axes[6] == -0.0 and message.axes[7] == -0.0):
-            self.last_joystick_command = self.BREAK_COMMAND
+        image_file_postfix = str(time.time())
+        image_file_name = "snapshot_" + image_file_postfix + ".jpeg"
 
-    def image_callback(self, message):
-        try:
-	    np_arr = np.fromstring(message.data, np.uint8)
-            cv2_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-    	except CvBridgeError, e:
-            rospy.logerror(e)
-    	else: 
-            training_data_folder = '../training_data/'
- 	    training_data_file_path = training_data_folder + 'training_data.csv'
+        cv2.imwrite(training_data_folder + image_file_name, last_image)
 
-            append_write = 'w'
-
-	    if os.path.exists(training_data_file_path):
-                append_write = 'a'
-
-            image_file_postfix = time.strftime("%Y_%m_%d-%H_%M_%S_%s")
-	    image_file_name = 'snapshot_' + image_file_postfix + '.jpeg'
-            cv2.imwrite(training_data_folder + image_file_name, cv2_img)
-
-            with open(training_data_file_path, append_write) as csvfile:
-    	        csv_writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                csv_writer.writerow([image_file_name, self.last_joystick_command])
+        with open(training_data_file_path, append_write_flag) as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow([image_file_name, last_joystick_command])
 
     def __init__(self):
-        rospy.init_node('camera_subscriber', log_level=rospy.DEBUG)
-        rospy.Subscriber('/raspicam_node/image/compressed', CompressedImage, self.image_callback, queue_size = 1)
+        rospy.init_node("training_data_controller", log_level=rospy.DEBUG)
 
-        rospy.Subscriber('joy', Joy, self.joystick_callback)
+        camera_node_subscriber = Subscriber("/raspicam_node/image/compressed", CompressedImage)
+        joystick_subscriber = Subscriber("joy", Joy)
 
-if __name__ == '__main__':
+        approx_time_synchronizer = ApproximateTimeSynchronizer([camera_node_subscriber, joystick_subscriber], 10, 1)
+        approx_time_synchronizer.registerCallback(self.approx_time_synchronizer_callback)
+
+if __name__ == "__main__":
     controller = TrainingDataController()
-    
+
     rospy.spin()
