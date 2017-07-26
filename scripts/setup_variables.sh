8002f286100554964609fdeba722c1fdd639813c
Bogdan Djukic <bdjukic@gmail.com>
2017-07-26 15:15:24 +0200

    Updating ROS master IP address

--- scripts/setup_variables.sh
+++ scripts/setup_variables.sh
@@ -4,8 +4,11 @@ echo
 echo Setting up ROS environment variables:
 echo
 
+# IP address for the robot car
 export ROS_IP="192.168.0.17"
-export ROS_MASTER_URI="http://192.168.0.20:11311"
+
+# IP address for the machine which does training and inference
+export ROS_MASTER_URI="http://192.168.0.10:11311"
 
 echo ROS_IP=$ROS_IP
 echo ROS_MASTER_URI=$ROS_MASTER_URI
